6d58a3806027b3bf5291215c50738269b360fe11
gilbk <gilbk@riseup.net>
2016-11-28 14:41:23 -0200

    Create mdmv_conecta.py

---
+++ mdmv_conecta.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import imaplib, getpass
+
+imap_host = 'imap.riseup.net'
+imap_user = mdmv
+imap_pass = getpass.getpass('senha: ')
+
+## open a connection 
+#imap = imaplib.IMAP4_SSL(imap_host)
+
+## login
+#imap.login(imap_user, imap_pass)
+
+
+#imap.close()
